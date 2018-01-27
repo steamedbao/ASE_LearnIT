89f247c7a5fc2dcdf4aba561f35fbae8546c1a4b
Milos Jankovic <jankovic.m87@gmail.com>
2018-01-27 19:36:35 +0100

    message

--- dj_forum/settings.py
+++ dj_forum/settings.py
@@ -108,6 +108,8 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 ALLOWED_HOSTS = ['*']
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/assets/'
