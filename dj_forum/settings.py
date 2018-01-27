f381fbf440c5c2af0b79e9f56392220cd7942434
Milos Jankovic <jankovic.m87@gmail.com>
2018-01-27 14:07:37 +0100

    trying to fix 500 error on heroku

--- dj_forum/settings.py
+++ dj_forum/settings.py
@@ -117,7 +117,7 @@ DATABASES['default'].update(dj_database_url.config(conn_max_age=500))
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Allow all host headers
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
